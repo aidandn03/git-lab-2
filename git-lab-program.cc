ab23714f7e3c238dc5ef009c35b88fa4930577a3
aidandn03 <60720254+aidandn03@users.noreply.github.com>
2024-05-17 23:20:11 -0400

    Add files via upload

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Aidan Nelson
+ *        Date: 5.17.2024
+ * Description: Hello Git program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
